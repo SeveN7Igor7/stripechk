367376c68ae7f8454910837166e512dd53d4d934
SeveN7Igor7 <igor_vinicius.carvalho@somosicev.com>
2024-03-13 01:26:05 -0300

    Atualizei alguns códigos

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+// Defina a chave pública e a chave secreta do Stripe
+$stripePublicKey = "pk_live_51OtgeFDvkxsAUVgYvDdU9n8kGTnP2TqegfCiGr76jeXu5bNGjdTfKtgQy5TFZHmGyQeTwIdZhxPartt2MBZCAsHX00JXkCPdoY";
+$stripeSecretKey = "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";
+?>
